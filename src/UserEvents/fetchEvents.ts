09f45d46a349baa01406fc41b29c1e072b8c6f4c
CRIMX <straybugs@gmail.com>
2025-02-11 15:19:26 +0800

    refactor: remove log

--- src/UserEvents/fetchEvents.ts
+++ src/UserEvents/fetchEvents.ts
@@ -14,7 +14,6 @@ export interface FetchStatus {
 }
 
 export const fetchEvents = (username: string, token?: string, page = 1): Observable<FetchStatus> => {
-  console.log("fetchEvents", page);
   const headers: HeadersInit = {
     Accept: "application/vnd.github+json",
   };
